a6bf8ff5ae69ad823870c4ea912ef43d3590fa98
Sachintha Madushan <49426927+sachinthamadushan@users.noreply.github.com>
2024-10-29 13:53:52 +0530

    initial setup

---
+++ main.js
@@ -0,0 +1,7 @@
+function saveCategory() {
+   let category = document.getElementById("category").value;
+document.getElementById.getElementById("categoryForm").addEventListner("submit", (e) => {
+    e.preventDefault();
+});
+     
+}
---
+++ pages/item_manage.php
@@ -0,0 +1,32 @@
+<div class="row">
+    <div class="col-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Manage Category</h5>
+                <form id="categoryForm">
+                    <label class="form-label">Category</label>
+                    <input type="text" class="form-control" id="category">
+                    <button onclick="saveCategory();" type="submit" class="btn btn-primary mt-3">Create</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="col-6">
+        <table class="table">
+            <thead>
+                <th>ID</th>
+                <th>Category</th>
+                <th></th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Vegetable</td>
+                    <td><a href="#" class="btn btn-primary">Edit</a>
+                        <a href="#" class="btn btn-danger">Delete</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
